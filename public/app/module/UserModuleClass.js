aeb435a7a0fad04a1063f34740d70e9fbfc6cec9
Tony Findeisen <github@it-ony.de>
2013-03-11 23:08:45 +0100

    load github user for overview page

--- public/app/module/UserModuleClass.js
+++ public/app/module/UserModuleClass.js
@@ -1,6 +1,43 @@
-define(["app/module/ModuleBase"], function(ModuleBase) {
+define(["app/module/ModuleBase", "github/model/User", "js/data/DataSource", "flow"], function(ModuleBase, User, DataSource, flow) {
 
     return ModuleBase.inherit("app.module.OverviewClass", {
+
+        defaults: {
+            user: null
+        },
+
+        inject: {
+            dataSource: DataSource
+        },
+
+        start: function(callback, routeContext) {
+
+            var self = this;
+
+            self.set("user", null);
+
+            flow()
+                .seq("user", function(cb) {
+
+                    var userName = routeContext.params[0];
+
+                    if (self.$.user && self.$.user.$.login !== userName) {
+                        self.set("user", null);
+                    }
+
+                    var user = self.$.dataSource.createEntity(User, userName);
+                    user.fetch(null, cb);
+
+                    return user;
+                })
+                .seq(function() {
+                    self.set("user", this.vars.user);
+                })
+                .exec(function(err) {
+                    err && console.error(err);
+                    callback && callback();
+                });
+        }
 
     });
 });

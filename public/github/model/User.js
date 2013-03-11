2c507a807ad9c65a20f247ded7f8255a2ee1a5ca
Tony Findeisen <github@it-ony.de>
2013-03-11 19:47:25 +0100

    reference repositories

--- public/github/model/User.js
+++ public/github/model/User.js
@@ -1,4 +1,4 @@
-define(["js/data/Model"], function(Model) {
+define(["js/data/Model", "js/data/Collection", "github/model/Repository"], function(Model, Collection, Repository) {
     return Model.inherit("github.model.User", {
 
         schema: {
@@ -6,8 +6,12 @@ define(["js/data/Model"], function(Model) {
             login: String,
 
             avatar_url: String,
-            public_repos: Number
+            public_repos: Number,
 
+            repositories: {
+                type: Collection.of(Repository),
+                key: "repos_url"
+            }
         }
 
     });

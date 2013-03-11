59f2bb7046105920b8f83fd85b37188c7ed56c09
Tony Findeisen <github@it-ony.de>
2013-03-11 19:46:21 +0100

    added necessary hacks for github api

--- public/github/data/GithubDataSourceClass.js
+++ public/github/data/GithubDataSourceClass.js
@@ -1,28 +1,63 @@
-define(["js/data/RestDataSource"], function (RestDataSource) {
+define(["js/data/RestDataSource", "js/data/DataSource", "js/data/Model", "underscore", "js/data/Collection"], function (RestDataSource, DataSource, Model, _, Collection) {
 
-    return RestDataSource.inherit("github.data.GithubDataSourceClass", {
+    var Processor = DataSource.Processor.inherit("github.data.GitHubDataSourceClass.Processor", {
+
+        _getValueForKey: function (data, key, schemaType, schemaDefinition) {
+
+            if (schemaDefinition && schemaDefinition.key) {
+                key = schemaDefinition.key;
+            }
+
+            if (_.isString(data) && schemaType.classof(Model)) {
+                return {
+                    id: data.split("/").pop(),
+                    href: data
+                }
+            }
+
+            if (schemaType.classof(Collection) && _.isString(data[key])) {
+                return {
+                    href: data[key]
+                };
+            }
+
+            if (schemaType.classof(Model)) {
+                var referenceKey = key + "_url";
+                if (data[referenceKey]) {
+                    return data[referenceKey];
+                }
+            }
+
+            return this.callBase();
+        }
+
+    });
+
+    var GithubDataSourceClass = RestDataSource.inherit("github.data.GithubDataSourceClass", {
 
         defaults: {
             accessToken: null
         },
 
+        $defaultProcessorFactory: Processor,
+
         getQueryParameters: function() {
             var params = this.callBase() || {};
-
             params["access_token"] = this.$.accessToken;
-
             return params;
         },
 
         getPathComponentsForModel: function (model) {
-            var ret = this.callBase();
 
-            if (ret && ret.length === 2 && model.isUniqueModel) {
-                ret.splice(1, 1);
+            if (model.constructor.name === "github.model.Me") {
+                return ["user"];
             }
 
-            return ret;
-        },
-
+            return this.callBase();
+        }
     });
+
+    GithubDataSourceClass.Processor = Processor;
+
+    return GithubDataSourceClass;
 });

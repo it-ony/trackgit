a5de40c9286b83eb617958b4456fabdacb225452
Tony Findeisen <github@it-ony.de>
2013-03-11 17:04:23 +0100

    added Model for Repository

---
+++ public/github/model/Repository.js
@@ -0,0 +1,18 @@
+define(["js/core/Model"], function(Model) {
+    return Model.inherit("github.model.Repository", {
+
+        schema: {
+            name: String,
+            description: String,
+            full_name: String,
+
+            "private": Boolean,
+            fork: false,
+            html_url: String,
+
+            has_issues: Boolean,
+            open_issues: Number
+        }
+
+    });
+});

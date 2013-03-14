7031a8534b7beee319105019ea0afc504847b73f
Tony Findeisen <github@it-ony.de>
2013-03-14 10:36:59 +0100

    create open and closed milestone collection

--- public/github/model/Repository.js
+++ public/github/model/Repository.js
@@ -1,4 +1,4 @@
-define(["js/data/Model", "js/data/Collection", "github/model/Issue", "github/model/User", "github/model/MileStone"], function(Model, Collection, Issue, User, MileStone) {
+define(["js/data/Model", "js/data/Collection", "github/model/Issue", "github/model/User", "github/model/MileStone", "js/data/Query"], function (Model, Collection, Issue, User, MileStone, Query) {
     return Model.inherit("github.model.Repository", {
 
         schema: {
@@ -16,6 +16,23 @@ define(["js/data/Model", "js/data/Collection", "github/model/Issue", "github/mod
             open_issues: Number,
             issues: Collection.of(Issue),
             mileStones: Collection.of(MileStone)
+        },
+
+        defaults: {
+            openMileStones: null,
+            closedMileStones: null
+        },
+
+        _commitMileStones: function (mileStones) {
+
+            if (!mileStones) {
+                return;
+            }
+
+            this.set({
+                openMileStones: mileStones.query(Query.query().eql("status", "open")),
+                closedMileStones: mileStones.query(Query.query().eql("status", "closed"))
+            });
         },
 
         idField: "name"

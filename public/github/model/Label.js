4808a7e9fb76ede0e62595257d6dcc77e8b8e8d5
Tony Findeisen <github@it-ony.de>
2013-03-23 13:01:07 +0100

    added logic for statuslabel

--- public/github/model/Label.js
+++ public/github/model/Label.js
@@ -1,4 +1,8 @@
 define(["js/data/Model"], function(Model) {
+
+    var undefined,
+        statusLabelExtractor = /^(?:(\d+)\s?-)?\s*(.*)$/;
+
     return Model.inherit("github.model.Label", {
 
         schema: {
@@ -6,6 +10,35 @@ define(["js/data/Model"], function(Model) {
             color: String
         },
 
-        idField: "name"
+        defaults: {
+            _name: null,
+            priority: null
+        },
+
+        idField: "name",
+
+        _commitName: function(name) {
+
+            if (name) {
+                var match = statusLabelExtractor.exec(name);
+                this.set({
+                    priority: match[1],
+                    _name: match[2]
+                });
+            } else {
+                this.set({
+                    _name: null,
+                    priority: null
+                });
+            }
+        },
+
+        name: function() {
+            return this.$._name;
+        }.onChange("_name"),
+
+        identifyStatus: function() {
+            return this.$.priority || this.$.priority == 0;
+        }.onChange("_priority")
     });
 });

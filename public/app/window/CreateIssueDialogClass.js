ccc9401091f25ecf37205865a57f0d651bf2324d
Tony Findeisen <github@it-ony.de>
2013-03-24 12:04:28 +0100

    set dialog size to wide

--- public/app/window/CreateIssueDialogClass.js
+++ public/app/window/CreateIssueDialogClass.js
@@ -2,8 +2,8 @@ define(["xaml!js/ui/Dialog", "js/core/I18n"], function(Dialog, I18n) {
     return Dialog.inherit("app.window.CreateIssueDialogClass", {
         defaults: {
             issue: null,
-
-            title: "{i18n.t('issue.dialog.title')}"
+            title: "{i18n.t('issue.dialog.title')}",
+            size: "wide"
         },
 
         inject: {
